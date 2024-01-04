51e5a6d5da16ec65f9d6d01141122f0194d7e647
Bálint Dózsa <balint.dozsa@cheppers.com>
2024-01-04 12:19:01 +0100

    Fix manageDeclarationPartition & clean

--- src/NavEvat/Api10/ManageDeclarationPartitionRequestXml.php
+++ src/NavEvat/Api10/ManageDeclarationPartitionRequestXml.php
@@ -8,11 +8,26 @@ class ManageDeclarationPartitionRequestXml extends BaseRequestXml
 {
     use AddQueryData;
 
-    public function __construct($config, $declarationUploadId, $partition)
+    private $hash;
+
+    public function __construct($config, $declarationUploadId, $partition, $hash)
     {
+        $this->hash = $hash;
+
         parent::__construct("ManageDeclarationPartitionRequest", $config);
 
         $this->xml->addChild('declarationUploadId', $declarationUploadId);
         $this->xml->addChild('partition', $partition);
+    }
+
+    protected function getRequestSignatureString()
+    {
+        $string = "";
+        $string .= $this->requestId;
+        $string .= preg_replace("/\.\d{3}|\D+/", "", $this->timestamp);
+        $string .= $this->config->user["signKey"];
+        $string .= $this->hash;
+
+        return $string;
     }
 }
